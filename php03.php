2937cb87ced5b56aabbd03c357312b83e4772382
MariiArjus <marii@hkhk.edu.ee>
2025-09-19 09:48:27 +0300

    Update php03.php

--- php03.php
+++ php03.php
@@ -7,43 +7,25 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
   </head>
   <body>
-    <h1>harjutus 9</h1>
-<?php
-    function tervita($n){
-        $puhastatud = ucfirst(strtolower(trim($n)));
-        return $puhastatud;
-    }
+<h1>Harjutus 03</h1>
+      <form action="#" method="get">
+        a <input type="number" name="a"  required><br>
+        b <input type="number" name="b" required><br>
+        h <input type="number" name="h" required><br>
+        <input type="submit" value="arvuta">
+      </form>
+        <?php
+          $a = $_GET["a"];
+          $b = $_GET["b"];
+          $h = $_GET["h"];
+          $s = ($a + $b)/2*$h;
 
-    echo tervita(" niMI  ")
-?>
+          echo "Trapetsi pindala: ".$s."<br>";
 
-
-<?php
-    echo "<br>";
-    $i = "stalker";
-    $punktid = strtoupper(implode('.', str_split($i)));
-    echo $punktid
-?>
-
-<?php
-    echo "<br>";
-
-function emaili_loomine($e, $p){
-    $tapikad = array("ä", "ö", "õ", "ü");
-    $asendus = array("a", "o", "o", "y");
-    $enimi = str_replace($tapikad, $asendus, mb_strtolower(trim($e)));
-    $pnimi = str_replace($tapikad, $asendus, mb_strtolower(trim($p)));
-
-    return $enimi.".".$pnimi."@hkhk.edu.ee";
-
-}
-
-echo emaili_loomine("üöõä", "zdg");
-?>
-
-
+        ?>
 
   
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
   </body>
+
 </html>
