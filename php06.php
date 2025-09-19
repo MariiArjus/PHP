6ead2cadf5246149efb1e6c25b51e160018f8e9d
MariiArjus <marii@hkhk.edu.ee>
2025-09-19 09:45:23 +0300

    Update php06.php

--- php06.php
+++ php06.php
@@ -56,6 +56,65 @@
     </div>
 
 
+	  <h1>harjutus6</h1>
+  <h2>Geneeriminme</h2>
+  <?php
+  //kolmega jagunev
+        for ($i=1; $i <= 100; $i++) {
+            if ($i%3==0) {
+             echo "<br>";
+        }
+        echo $i. "<br>";  
+    }
+
+
+  //kahanev
+    for ($i=10; $i <= 0; $i--) {
+        echo $i. "<br>";  
+    }
+
+
+
+
+    echo "<br>";
+
+
+  //10x10 tärnid
+    for ($i=1; $i <= 10; $i++) {
+        echo " *";
+        if ($i%10==0) {
+            echo "<br>";
+        }
+    }
+
+    echo "<br>";
+
+
+  //tärnidega read
+    for ($i=1; $i <= 0; $i++) {
+        echo "*";
+    }
+
+    for ($i=1; $i <= 0; $i++) {
+        echo "*<br>";
+    }
+
+
+    echo "<br>";
+
+
+  	for ($i=1; $i <= 100; $i++) {
+		echo $i. ". ";	
+        if ($i%10==0) {
+            echo "<br>";
+        }
+
+	}
+
+
+?>
+
+
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
   </body>
 </html>
