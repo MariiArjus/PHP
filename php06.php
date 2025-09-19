d9f7da1dd41a0bc07745d298abe8d4595c6c929f
MariiArjus <marii@hkhk.edu.ee>
2025-09-19 09:42:09 +0300

    Create php06.php

---
+++ php06.php
@@ -0,0 +1,61 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Harjutus 01</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+  </head>
+  <body>
+
+<h1>harjutus6</h1>
+  <h2>Geneeriminme</h2>
+  <?php
+  	for($nr=1;$nr<=100;$nr++){
+		echo $nr;	
+	}
+
+
+// – loo arvud 1-100
+// – loo pärast iga 10 arvu reavahetus
+// – lisa iga arvu järele punkt (N: 1. 2. 3. jne…)
+
+  ?>
+
+
+
+ <div class="container">
+      <div class="row">
+        <div class="col-sm-4">
+          <img src="/img/<?php echo $pildid[2]; ?> alt="">
+      </div>
+</div>
+
+<div class="row">
+  <?php
+
+    foreach ($pildid as $pilt) {
+      echo '<div class="col-sm-2">';
+    echo '<img class="img-fluid" src="/img/'" .$pilt. '" alt="">'';
+    echo '</div>';
+
+      }
+    ?>
+          </div>
+</div>
+
+
+
+
+ <div class="container">
+      <div class="row">
+        <div class="col-sm-4"><img src="/img/devlin.jpg" alt=""></div>
+        <div class="col-sm-4"><img src="/img/freeland.jpg"alt=""></div>
+        <div class="col-sm-4"><img src="/img/freeland.jpg"alt=""></div>
+      </div>
+    </div>
+
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
+  </body>
+</html>
